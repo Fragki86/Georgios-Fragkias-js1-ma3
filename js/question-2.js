677d167b40a98f415b958e3813136fe949aedcf6
Georgios Fragkias <geo.fragkias@gmail.com>
2021-02-05 22:25:56 +0100

    send5

--- js/question-2.js
+++ js/question-2.js
@@ -1,24 +1,4 @@
-
-
 // Question 2
-/*
-Make a call to the following API endpoint:
-https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating
-
-Loop through the results and display the following properties in HTML, but only for the first eight results:
- - name
- - rating
- - number of tags (not the tag details, just the amount of tags)
-The styling for this assignment is not important but loading indicator should be displayed while the API call is in progress.
-
-Be sure to handle any potential errors in the code.
-
-You can use either regular promise or async/await syntax to make the call.
-
-Be sure to arrange all file types appropriately, consult the repos from the lessons for examples.
-*/
-
-
 
 const API_URL = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 const showResults = document.querySelector(".results");
@@ -33,7 +13,7 @@ async function getAPI() {
         for (let i = 0; i < 8; i++) {
             const amountOfTags = listOfGames[i].tags;
 
-            showResults.innerHTML += `<div>${listOfGames[i].name}: ${listOfGames[i].rating}, ${amountOfTags.length}</div>`;
+            showResults.innerHTML += `<div><b>${listOfGames[i].name}:</b> ${listOfGames[i].rating} / Tags: ${amountOfTags.length}</div>`;
         }
     } catch (error) {
         showResults.innerHTML = `Something is wrong dude!`
@@ -42,17 +22,3 @@ async function getAPI() {
 }
 
 getAPI();
-
-
-
-
-// fetch("https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating")
-//     .then(function(response) {
-//         console.log(response)
-//     })
-//     .then(function(results) {
-//         console.log(results.length);
-//     })
-//     .catch(function(error) {
-//         console.log(error);
-//     });
